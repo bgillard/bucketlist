bedfb8a0a772feb7d503090fc4c1bf0bc3520d5d
Bradley E. Gillard <bradleygillard@gmail.com>
2011-11-29 14:19:59 -0600

    Updating links to ruby style.

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,8 @@ Bucketlist::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  map.root :controller => "home"
+  root :to => "home"
 
   # See how all your routes lay out with "rake routes"
 
