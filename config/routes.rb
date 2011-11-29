3cd187305ee2e8d3970097131a7bf9132cd2a469
Bradley E. Gillard <bradleygillard@gmail.com>
2011-11-29 14:35:12 -0600

    Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Bucketlist::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  map.root :controller => "home"
+  # map.root :controller => "home"
   root :to => "home"
 
   # See how all your routes lay out with "rake routes"
